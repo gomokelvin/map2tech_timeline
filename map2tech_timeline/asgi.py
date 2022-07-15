dfa036b2d943fdb9fbbc980b1bf498ce02048194
Kelvin Gomo <kelvingomo@gmail.com>
2022-07-16 01:43:32 +0200

    First commit.

---
+++ map2tech_timeline/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for map2tech_timeline project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'map2tech_timeline.settings')
+
+application = get_asgi_application()
